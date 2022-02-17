58594c39794f49881bdd4e0cefb2785d8b2d43ad
Shannon Lewis <masumi768@Shannons-Air.local>
2022-02-16 22:15:15 -1000

    Update requirements

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from flask_gravatar import Gravatar
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from functools import wraps
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -215,4 +216,8 @@ def delete_post(post_id):
     return redirect(url_for('get_all_posts'))
 
 
+if __name__ == "__main__":
+    # For HEROKU
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
 
